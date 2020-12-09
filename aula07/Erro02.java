c76aa584c780aed9d7a65b291da38587f9150e34
Rafael Souza <rafaelsnt123@gmail.com>
2020-12-09 01:43:25 -0300

    Código da aula 07

---
+++ aula07/Erro02.java
@@ -0,0 +1,8 @@
+class Erro02 {
+	public static void main(String[] args){
+
+		int cont; // Erro de Semântica
+
+		System.out.println("Hello World " + cont);
+	}
+}
---
+++ aula07/Erro03.java
@@ -0,0 +1,6 @@
+class Erro01 {
+	public static void Main(String[] args){ // Erro em tempo de execução
+
+		System.out.println("Hello World");
+	}
+}
